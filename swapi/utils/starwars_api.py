aa568d7d9d8347177d5a5f170d938a74e484e807
t1m4 <ruslan.hamidullin@starnavi.io>
2021-08-04 17:24:55 +0300

    Modify StarwarsApi client using generators

--- swapi/utils/starwars_api.py
+++ swapi/utils/starwars_api.py
@@ -1,9 +1,10 @@
 import logging
 import math
 import time
-from typing import List
 
 import requests
+
+from swapi.utils.petl_utils import CSVWriter
 
 logger = logging.getLogger('starwars.console_logger')
 
@@ -11,44 +12,6 @@ logger = logging.getLogger('starwars.console_logger')
 class StarwarsApi:
     PEOPLE_URL = 'https://swapi.dev/api/people'
     PAGE_TIMEOUT = None  # None or int
-
-    def get_page_persons(self, url: str) -> (List, str):
-        """
-        Get each page people only with required information.
-        """
-        response = self.__get_json(url)
-        if response:
-            for person in response['results']:
-                # change homeworld
-                homeworld_response = self.__get_json(person['homeworld'])
-                if homeworld_response:
-                    person['homeworld'] = homeworld_response['name']
-                else:
-                    person['homeworld'] = None
-                # change edited date
-                person['date'] = person['edited'][:10]
-                # delete
-                del person['films']
-                del person['species']
-                del person['vehicles']
-                del person['starships']
-                del person['edited']
-                del person['created']
-                del person['url']
-            return response['results']
-        else:
-            return []
-
-    def get_people_urls(self, function):
-        """
-        Get all people pages url
-        """
-        response = self.__get_json(self.PEOPLE_URL)
-        count_in_first = len(response['results'])
-        count_of_person = response['count']
-        count_of_pages = math.ceil(count_of_person / count_in_first)
-
-        return [function.s(self.PEOPLE_URL + "?page=" + str(i)) for i in range(1, count_of_pages + 1)]
 
     def __get_json(self, url: str):
         """
@@ -66,10 +29,66 @@ class StarwarsApi:
         else:
             logger.info(f'Status code for {url}: {r.status_code}')
 
+    def get_people_urls(self):
+        """
+        Get all people pages url. Make it only one time
+        """
+        response = self.__get_json(self.PEOPLE_URL)
+        count_in_first = len(response['results'])
+        count_of_person = response['count']
+        count_of_pages = math.ceil(count_of_person / count_in_first)
+
+        return [(self.PEOPLE_URL + "?page=" + str(i)) for i in range(1, count_of_pages + 1)]
+
+    def get_people_list(self):
+        urls = self.get_people_urls()
+        for url in urls:
+            response = self.__get_json(url)
+            if response:
+                results = response['results']
+                yield results
+
+    def get_person_information(self, films, type):
+        types = {
+            'films': 'title',
+            'species': 'name',
+            'vehicles': 'name',
+            'starships': 'name',
+        }
+        field = types[type]
+        results = []
+        for film_url in films:
+            response = self.__get_json(film_url)
+            if response:
+                results.append(response[field])
+        return results
+
+
+def people_dataset():
+    api_client = StarwarsApi()
+    start_time = time.time()
+    for page in api_client.get_people_list():
+        start_time = time.time()
+        for person in page:
+            result = {}
+            result['name'] = person.get('name')
+            result['height'] = person.get('height')
+            result['mass'] = person.get('mass')
+            result['hair_color'] = person.get('hair_color')
+            result['skin_color'] = person.get('skin_color')
+            result['eye_color'] = person.get('eye_color')
+            result['gender'] = person.get('gender')
+            result['films'] = api_client.get_person_information(person.get('films'), type='films')
+            result['species'] = api_client.get_person_information(person.get('species'), type='species')
+            result['starships'] = api_client.get_person_information(person.get('starships'), type='starships')
+            yield result
+        print(time.time() - start_time)
+        print('hello', type(page))
+    print(time.time() - start_time)
+
 
 if __name__ == '__main__':
-    s = StarwarsApi()
-    start_time = time.time()
-    r = s.get_people_urls()
-    print(r)
-    print(time.time() - start_time)
+    csv_writer = CSVWriter()
+    for person in people_dataset():
+        print(person)
+        csv_writer.write('../../example.csv', person)
