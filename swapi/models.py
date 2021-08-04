a9fae1feef887d5bd02225840f60a7650b69d9ef
t1m4 <ruslan.hamidullin@starnavi.io>
2021-08-04 11:26:48 +0300

    Add File model for saving csv file's metadata.

--- swapi/models.py
+++ swapi/models.py
@@ -1,3 +1,9 @@
+from fileinput import filename
+
 from django.db import models
 
 # Create your models here.
+class File(models.Model):
+    filename = models.CharField(max_length=40, unique=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    count_of_pages = models.IntegerField()
