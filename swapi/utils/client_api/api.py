16bb492744943d270f1ff45a3a93ba146a51ce86
t1m4 <ruslan.hamidullin@starnavi.io>
2021-08-05 17:39:52 +0300

    Add method for getting all person tools using list of ids

--- swapi/utils/client_api/api.py
+++ swapi/utils/client_api/api.py
@@ -1,8 +1,8 @@
 import logging
+import re
 
 import requests
 import simplejson
-from simplejson import JSONDecodeError
 
 from swapi.utils.client_api.exceptions import ClientAPIException
 from swapi.utils.petl_utils import CSVWriter
@@ -12,17 +12,19 @@ logger = logging.getLogger('starwars.console_logger')
 
 class ClientAPI:
     PEOPLE_URL = 'https://swapi.dev/api/people'
-    FIlM_URL = 'https://swapi.dev/api/films/{id}/'
-    SPECIE_URL = 'https://swapi.dev/api/species/{id}/'
-    VEHICLE_URL = 'https://swapi.dev/api/vehicles/{id}/'
-    STARSHIP_URL = 'https://swapi.dev/api/starship/{id}/'
 
     PAGE_TIMEOUT = None
-    PERSON_INFO_TYPES = {
+    TYPES_OF_TOOL_FIELDS = {
         'films': 'title',
         'species': 'name',
         'vehicles': 'name',
         'starships': 'name',
+    }
+    TYPES_OF_TOOL_URL = {
+        'films': 'https://swapi.dev/api/films/{id}/',
+        'species': 'https://swapi.dev/api/species/{id}/',
+        'vehicles': 'https://swapi.dev/api/vehicles/{id}/',
+        'starships': 'https://swapi.dev/api/starships/{id}/',
     }
 
     def __get_json(self, url: str, params=None, timeout=None):
@@ -58,43 +60,64 @@ class ClientAPI:
                 break
         return result
 
-    def get_person_information(self, info_urls: list, type: str):
+    def get_person_tool_by_id(self, id: int, type_of_tool: str):
+        try:
+            url = self.TYPES_OF_TOOL_URL[type_of_tool]
+        except:
+            raise ValueError("Can't find type of person tool: " + type_of_tool)
+        url = url.format(id=id)
+        return self.__get_json(url)
+
+    def get_person_tools(self, ids: list, type_of_tool: str):
         """
         Get person films, species, vehicles and starships
         """
+        try:
+            field = self.TYPES_OF_TOOL_FIELDS[type_of_tool]
+        except:
+            raise ValueError("Can't find type of person tool: " + type_of_tool)
 
-        field = self.PERSON_INFO_TYPES[type]
         results = []
-        for url in info_urls:
-            try:
-                response = self.__get_json(url)
-            except ClientAPIException as error:
-                logger.info(error)
-                continue
-            else:
-                results.append(response[field])
+        for id in ids:
+            response = self.get_person_tool_by_id(id, type_of_tool)
+            results.append(response[field])
         return results
 
 
-    def get_film_by_id(self, id: int):
-        url = self.FIlM_URL.format(id=id)
-        return self.__get_json(url)
+def get_id_from_url(url: str):
+    """
+    Get object id from given url
+    :param url:
+    :return:
+    """
+    id_pattern = r'/(\d+)/?$'
+    result = re.search(id_pattern, url)
+    if result:
+        return int(result.group(1))
+    else:
+        # TODO what if don't find it. Are you sure about it?
+        raise ValueError("Can't find id in url")
 
-    def get_specie_by_id(self, id: int):
-        url = self.SPECIE_URL.format(id=id)
-        return self.__get_json(url)
 
-    def get_vehicle_by_id(self, id: int):
-        url = self.VEHICLE_URL.format(id=id)
-        return self.__get_json(url)
+def get_all_person_tool_ids(films_url: list):
+    """
+    Get list of ids from list of urls
+    """
+    result = []
+    for i in films_url:
+        id = get_id_from_url(i)
+        result.append(id)
+    return result
 
-    def get_starship_by_id(self, id: int):
-        url = self.STARSHIP_URL.format(id=id)
-        return self.__get_json(url)
 
 def people_dataset():
     api_client = ClientAPI()
-    for person in api_client.get_people_list(1, 10):
+    try:
+        data = api_client.get_people_list(1, 10)
+    except ClientAPIException as error:
+        return
+
+    for person in data:
         result = {}
         result['name'] = person.get('name')
         result['height'] = person.get('height')
@@ -103,15 +126,23 @@ def people_dataset():
         result['skin_color'] = person.get('skin_color')
         result['eye_color'] = person.get('eye_color')
         result['gender'] = person.get('gender')
-        result['films'] = api_client.get_film_by_id(person.get('films'), type='films')
-        result['species'] = api_client.get_person_information(person.get('species'), type='species')
-        result['starships'] = api_client.get_person_information(person.get('starships'), type='starships')
+
+        film_ids = get_all_person_tool_ids(person.get('films'))
+        result['films'] = api_client.get_person_tools(film_ids, type_of_tool='films')
+
+        species_ids = get_all_person_tool_ids(person.get('species'))
+        result['species'] = api_client.get_person_tools(species_ids, type_of_tool='species')
+
+        vehicles_ids = get_all_person_tool_ids(person.get('vehicles'))
+        result['vehicles'] = api_client.get_person_tools(vehicles_ids, type_of_tool='vehicles')
+
+        starships_ids = get_all_person_tool_ids(person.get('starships'))
+        result['starships'] = api_client.get_person_tools(starships_ids, type_of_tool='starships')
         yield result
 
 
 if __name__ == '__main__':
-    # csv_writer = CSVWriter()
-    # for person in people_dataset():
-    #     print(person)
-    #     csv_writer.write('../../example.csv', person)
-    api = ClientAPI()
+    csv_writer = CSVWriter()
+    for person in people_dataset():
+        print(person)
+        csv_writer.write('../../example.csv', person)
