4d1a94731f6b52ebca4ffbeca19787d792e560cc
t1m4 <ruslan.hamidullin@starnavi.io>
2021-08-06 16:28:40 +0300

    Fix tasks.py create model

--- swapi/tasks.py
+++ swapi/tasks.py
@@ -20,7 +20,6 @@ def task_get_all_in_csv():
 
     count = 0
     for person in people_dataset():
-        logger.info(person)
         csv_writer.write(filename, person)
         count += 1
     File.objects.create(filename=filename, count_of_people=count)
