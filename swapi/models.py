faeab82127027f73d521395959c4f5bd9967902f
t1m4 <ruslan.hamidullin@starnavi.io>
2021-08-06 15:20:29 +0300

    Add people count fo the File model

--- swapi/models.py
+++ swapi/models.py
@@ -6,3 +6,4 @@ from django.db import models
 class File(models.Model):
     filename = models.CharField(max_length=40, unique=True)
     datetime = models.DateTimeField(auto_now_add=True)
+    count_of_people = models.IntegerField()
--- swapi/tasks.py
+++ swapi/tasks.py
@@ -17,7 +17,10 @@ def task_get_all_in_csv():
     csv_writer = CSVWriter()
     random_string = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(20))
     filename = f'people_{random_string}.csv'
-    File.objects.create(filename=filename)
+
+    count = 0
     for person in people_dataset():
         logger.info(person)
         csv_writer.write(filename, person)
+        count += 1
+    File.objects.create(filename=filename, count_of_people=count)
